619df2700ac87b29ef930a971e8ecc318fe094aa
Simon Emms <simon@simonemms.com>
2015-11-01 20:05:53 +0000

    Imported angular cookie to file

--- src/ui-router.login.js
+++ src/ui-router.login.js
@@ -9,10 +9,10 @@
 
 
 /* Third-party modules */
-import "angular-cookie";
 
 
 /* Files */
+import "../bower_components/angular-cookie/angular-cookie.min.js";
 
 
 var app = angular.module("ui.router.login", [
